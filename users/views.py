2e1a35dd42b30c0265013725c86c4b101c9167f9
mslmyrtd <mslmyrtd@gmail.com>
2022-03-08 18:08:20 +0300

    view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+class RegisterAPI(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
