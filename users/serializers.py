c3551bccf63ae3c89bf557dfe33c6635dba72c03
mslmyrtd <mslmyrtd@gmail.com>
2022-03-09 18:23:19 +0300

    serializer update Token serializer added

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers, validators
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
+from dj_rest_auth.serializers import TokenSerializer
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
         required = True,
@@ -9,36 +10,49 @@ class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         write_only = True,
         required = True,
-        validtors = [validate_password]
+        validators = [validate_password],
+        style={"input_type":"password"}
     )
     password2 = serializers.CharField(
         write_only = True,
-        required = True
+        required = True,
+        style={"input_type":"password"}
     )
     class Meta:
         model = User
         fields = [
-            ‘username’,
-            ‘first_name’,
-            ‘last_name’,
-            ‘email’,
-            ‘password’,
-            ‘password2’
+            
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "password",
+            "password2"
         ]
         extra_kwargs = {
-            ‘password’: {‘write_only’ : True},
-            ‘password2’: {‘write_only’ : True},
+            "password": {"write_only" : True},
+            "password2": {"write_only" : True},
         }
     def create(self, validated_data):
-        password = validated_data.get(‘password’)
-        validated_data.pop(“password2”)
+        password = validated_data.get("password")
+        validated_data.pop("password2")
         user = User.objects.create(**validated_data)
         user.set_password(password)
         user.save()
         return user
     def validate(self, data):
-        if data[“password”] != data[“password2”]:
+        if data["password"] != data["password2"]:
             raise serializers.ValidationError(
-                {“password” : “Password fields didn’t match.“}
+                {"password" : "Password fields didn’t match."}
             )
         return data
+    
+class UserTokenSeriliazer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=("id","first_name","last_name","email")
+class CustomToken(TokenSerializer):
+    user=UserTokenSeriliazer(read_only=True)
+    
+    class Meta(TokenSerializer.Meta):
+        fields=("key", "user")
--- users/views.py
+++ users/views.py
@@ -1,7 +1,16 @@
 
+from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer
 class RegisterAPI(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+    
+    def post(self, request, *args, **kwargs):
+        seriliazer=RegisterSerializer(data=request.data)
+        seriliazer.is_valid(raise_exception=True)
+        seriliazer.save()
+        return Response(
+            {"message":"user created succesfully"}
+        )
