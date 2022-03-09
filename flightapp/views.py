032d213946cf6aac95b12b417316092fd09010a4
mslmyrtd <mslmyrtd@gmail.com>
2022-03-09 21:33:44 +0300

    FlightView added for all crud

--- flightapp/views.py
+++ flightapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import Flight, Reservation
+from .serializers import FlightSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class FlightView(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
