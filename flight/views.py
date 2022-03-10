016d04ba0c08d263be3524db3122d8a587c4183c
mslmyrtd <mslmyrtd@gmail.com>
2022-03-10 15:46:01 +0300

    update

--- flight/views.py
+++ flight/views.py
@@ -1,8 +1,9 @@
 from .models import Flight
 from .serializers import FlightSerializer
 from rest_framework import viewsets
-
+from .permissions import IsStuffOrReadOnly
 class FlightView(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+    permission_classes =(IsStuffOrReadOnly,)
     
