b91d528abfb33fa343a481d1b7cfbd51809be913
mslmyrtd <mslmyrtd@gmail.com>
2022-03-10 15:05:58 +0300

    urls created router added

---
+++ flight/serializers.py
@@ -0,0 +1,63 @@
+from asyncore import write
+from dataclasses import field
+from importlib_metadata import requires
+from rest_framework import serializers
+from .models import Flight, Passenger, Reservation
+
+
+class FlightSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Flight
+        fields = (
+            "id",
+            "flightNumber",
+            "operatingAirlines",
+            "departureCity",
+            "arrivalCity",
+            "dateOfDeparture",
+            "estimatedTimeOfDeparture",
+        )
+
+
+class PassengerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Passenger
+        fields = (
+            "id",
+            "firstName",
+            "lastName",
+            "email",
+            "phoneNumber",
+        )
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+    passenger = PassengerSerializer(many=True, required=False)
+    flight = serializers.PrimaryKeyRelatedField(queryset=Flight.objects.all())
+    # flight_id = serializers.IntegerField()
+    user = serializers.StringRelatedField()
+    user_id = serializers.IntegerField(write_only=True, required=False)
+
+    class Meta:
+        model = Reservation
+        fields = (
+            "id",
+            "flight",
+            "passenger",
+            "user",
+            "user_id",
+        )
+
+    def create(self, validated_data):
+        print(validated_data)
+        passenger_data = validated_data.pop('passenger')
+        print(passenger_data)
+        validated_data["user_id"] = self.context['request'].user.id
+        reservation = Reservation.objects.create(**validated_data)
+        for passenger in passenger_data:
+            pas = Passenger.objects.create(**passenger)
+            reservation.passenger.add(pas)
+        reservation.save()
+
+        return 
--- flight/views.py
+++ flight/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import Flight, Reservation
+from .serializers import FlightSerializer, ReservationSerializer, StaffFlightSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class FlightView(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+    # permission_classes = (IsStuffOrReadOnly,)
