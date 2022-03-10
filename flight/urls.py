73ba17a0db65313dc01bad874ebef2aa262c2dbf
mslmyrtd <mslmyrtd@gmail.com>
2022-03-10 15:16:48 +0300

    permission added for put delete cretae

--- flight/urls.py
+++ flight/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework import routers
-from .views import FlightView, ReservationView
+from .views import FlightView
 
 router = routers.DefaultRouter()
 router.register('flights', FlightView)
--- flight/views.py
+++ flight/views.py
@@ -1,8 +1,8 @@
-from .models import Flight, Reservation
-from .serializers import FlightSerializer, ReservationSerializer, StaffFlightSerializer
+from .models import Flight
+from .serializers import FlightSerializer
 from rest_framework import viewsets
 
 class FlightView(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
-    # permission_classes = (IsStuffOrReadOnly,)
+    
